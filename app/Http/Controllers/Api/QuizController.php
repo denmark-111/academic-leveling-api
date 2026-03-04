52d12371d34d3845c92a553e941f6bee6d3c7d03
denmark-111 <maglipon.denmarc.amar@gmail.com>
2026-03-04 10:24:02 +0800

    feat(quiz): add create, read, and update endpoints for quiz details

---
+++ app/Http/Controllers/Api/QuizController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreQuizRequest;
+use App\Http\Requests\UpdateQuizRequest;
+use App\Http\Resources\QuizResource;
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return QuizResource::collection(Quiz::where('is_public', true)->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuizRequest $request)
+    {
+        $validated = $request->validated();
+
+        $quiz = Quiz::create([
+            'user_id' => $request->user()->id,
+            ...$validated
+        ]);
+
+        return QuizResource::make($quiz)
+            ->additional(['message' => 'Quiz created successfully'])
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, Quiz $quiz)
+    {
+        if (!$quiz->is_public && $quiz->user_id !== $request->user()->id) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        return QuizResource::make($quiz);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuizRequest $request, Quiz $quiz)
+    {
+        if ($quiz->user_id !== $request->user()->id) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $validated = $request->validated();
+
+        $quiz->update($validated);
+
+        return QuizResource::make($quiz)
+            ->additional(['message' => 'Quiz updated successfully']);   
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quiz $quiz)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('quizzes', QuizController::class)->only(['index', 'store', 'show', 'update']);
+});
